7c58274a0b807df2dc7e5a8890f84000a0ced61a
hanwj75 <wjws9608@gmail.com>
2024-10-10 00:13:10 +0900

    (한우종) Add:"회원가입/로그인"
    회원가입 로그인 router 추가

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
+import UserRouter from './routes/user.router.js';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
 const PORT = 3000;
 
+app.use(
+  cors({
+    origin: 'http://localhost:8080', // 클라이언트의 주소
+    exposedHeaders: ['Authorization'],
+  }),
+);
 app.use(express.json());
+app.use('/api', [UserRouter]);
 
 initSocket(server);
 
