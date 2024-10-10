6265fcb0e51994cb72784b3867542fe05dc9d46c
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-10-10 23:16:17 +0900

    (박건순) Update: "monsterSpawn 추가"

--- src/handler/monster.handler.js
+++ src/handler/monster.handler.js
@@ -1,5 +1,17 @@
+import { getData } from '../init/data.js';
 import { getMonsters, setMonsters } from '../models/monster.model.js';
 // 몬스터 소환 검증
+export const monsterSpawn = (accountId, data) => {
+  const { DBData } = getData();
+
+  const { monsterDBData } = DBData.monsters;
+  if (!monsters) {
+    return { status: 'fail', message: 'Monsters not found' };
+  }
+
+  setMonsters(accountId, data);
+  return { status: 'success', message: 'Monster killed' };
+};
 
 // 몬스터 사망시 검증
 export const monsterKill = (accountId, data) => {
