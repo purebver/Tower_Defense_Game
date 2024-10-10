f2527f6bb43e588cf6d76d09df8e345ed74f0f1d
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-10-10 15:15:25 +0900

    (박건순) Update: "몬스터 사망시 검증"

--- src/handler/handlerMapping.js
+++ src/handler/handlerMapping.js
@@ -1,10 +1,11 @@
 import { towerHandler } from './tower.handler.js';
-
+import { monsterKill } from './monster.handler.js';
 const handlerMappings = {
   // 1: gameStart 핸들러,
   // 2: gameEnd 핸들러,
   // 10: monster 관련 핸들러,
   // 20: stage 관련 핸들러,
+  11: monsterKill,
   30: towerHandler,
 };
 
--- src/models/monster.model.js
+++ src/models/monster.model.js
@@ -1,17 +1,17 @@
 const monsters = {};
 
-//유저의 몬스터 배열 생성
+//유저가 잡은 몬스터 기록 배열 생성
 export const createMonsters = (accountId) => {
   monsters[accountId] = [];
   console.log(`createMonsters`);
 };
 
-//몬스터 정보 받아오기
+//몬스터 기록 받아오기
 export const getMonsters = (accountId) => {
   return monsters[accountId];
 };
 
-//몬스터 초기 세팅
+//몬스터 세팅
 export const setMonsters = (accountId, monstersdata) => {
   console.log(`setMonster`);
   return monsters[accountId].push({ monstersdata });
