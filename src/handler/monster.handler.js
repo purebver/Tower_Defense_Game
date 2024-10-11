587fc110689e40cf22dc228c79633f1fd54936c0
keonsoonpark4798 <98qkrrjstns@gmail.com>
2024-10-11 11:46:48 +0900

    (박건순) Fix: "몬스터 스폰, 킬 검증 오류 수정"

--- src/handler/monster.handler.js
+++ src/handler/monster.handler.js
@@ -8,21 +8,12 @@ export const monsterSpawn = (accountId, data) => {
   //몬스터 DB 데이터
   const dbMonster = monsters.find((a) => a.monsterId === monster.monsterId);
 
+  if (!dbMonster) {
+    return { status: 'fail', message: 'monster not found' };
+  }
   // 몬스터 체력 검증
   if (monster.monsterHp !== dbMonster.monsterHp) {
     return { status: 'fail', message: 'monsterHp Is Strange' };
-  }
-  // 몬스터 공격력 검증
-  if (monster.monsterAttack !== dbMonster.monsterAttack) {
-    return { status: 'fail', message: 'monsterAttack Is Strange' };
-  }
-  // 몬스터 골드 검증
-  if (monster.monsterGold !== dbMonster.monsterGold) {
-    return { status: 'fail', message: 'monsterGold Is Strange' };
-  }
-  // 몬스터 스코어 검증
-  if (monster.monsterScore !== dbMonster.monsterScore) {
-    return { status: 'fail', message: 'monsterScore Is Strange' };
   }
   // 몬스터 스폰 시간 검증
   if (monster.spawnTime !== dbMonster.spawnTime) {
@@ -34,10 +25,21 @@ export const monsterSpawn = (accountId, data) => {
 
 // 몬스터 사망시 검증
 export const monsterKill = (accountId, data) => {
-  const monsters = getMonsters(accountId);
-  console.log('ddddd');
+  //몬스터 DB 데이터
+  const { monsters } = getData();
+  //클라이언트에서 스폰되는 몬스터 데이터
+  const monster = data.monster;
+  const dbMonster = monsters.find((a) => a.monsterId === monster.monsterId);
 
-  if (!monsters) {
+  //죽인 몬스터 배열
+  const monsterArr = getMonsters(accountId);
+  console.log('hi:' + dbMonster.monsterScore);
+  // 몬스터 스코어 검증
+  if (monster.monsterScore !== dbMonster.monsterScore) {
+    return { status: 'fail', message: 'monsterScore Is Strange' };
+  }
+
+  if (!monsterArr) {
     return { status: 'fail', message: 'monster not found' };
   }
 
