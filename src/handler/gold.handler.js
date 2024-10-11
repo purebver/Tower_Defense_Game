141fc1dd605d00309c9725977e2121c8c7b450e9
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-10-12 01:05:46 +0900

    (정진희) fix "타워 판매 포함, 골드 검증"
    타워를 판매해도 골드를 검증할 수 있다.
    기본 타워를 판매해도 검증 가능하다.

--- src/handler/gold.handler.js
+++ src/handler/gold.handler.js
@@ -1,11 +1,20 @@
 import { getData } from '../init/data.js';
 import { moneyBase } from '../models/stage.model.js';
 
-const goldCalculate = async (currentTower, monsterGold) => {
+let numOfInitialTowers = 3; // 초기 타워 개수
+let baseTowerCost = 0;
+
+export const baseTowerDelete = async (isDelete) => {
+  if (isDelete && numOfInitialTowers !== 0) {
+    numOfInitialTowers--;
+    baseTowerCost += 1000;
+  }
+};
+
+export const goldCalculate = (currentTower, monsterGold) => {
   let totalGold = 0;
   // 구매한 타워 총 가격
   let totalTowerGold = 0;
-  let numOfInitialTowers = 3; // 초기 타워 개수
 
   //console.log('----------------------');
   for (let i = numOfInitialTowers; i < currentTower.length; i++) {
@@ -14,9 +23,7 @@ const goldCalculate = async (currentTower, monsterGold) => {
     totalTowerGold += towerInfo.towerCost;
   }
 
-  totalGold = moneyBase + monsterGold - totalTowerGold;
+  totalGold = moneyBase + monsterGold - totalTowerGold + baseTowerCost;
 
   return totalGold;
 };
-
-export default goldCalculate;
